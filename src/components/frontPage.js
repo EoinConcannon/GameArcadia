11f754e366b4a2a076877f7ef7f66aa5ad39a4c2
EoinConcannon <G00406729@atu.ie>
2024-10-28 18:48:53 +0000

    added components folder and created frontPage.js

---
+++ src/components/frontPage.js
@@ -0,0 +1,22 @@
+//generated with copilot, refine later
+import React from 'react';
+import './frontPage.css';
+
+const FrontPage = () => {
+    return (
+        <div className="front-page">
+            <div className="top-border">
+                <button className="nav-button">Home</button>
+                <button className="nav-button">Store</button>
+            </div>
+            <div className="content">
+                {/* Add your main content here */}
+            </div>
+            <div className="bottom-border">
+                <button className="nav-button">About</button>
+            </div>
+        </div>
+    );
+};
+
+export default FrontPage;
