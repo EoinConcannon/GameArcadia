396410c6b4daa7ae7bee498015eb2fe350dc8fd4
EoinConcannon <G00406729@atu.ie>
2025-01-14 18:36:20 +0000

    created a blank admin page

---
+++ src/components/AdminPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPage = () => {
+
+    return (
+        <div className="front-page">
+            <p>This is the admin page</p>
+        </div>
+    );
+};
+
+export default AdminPage;
