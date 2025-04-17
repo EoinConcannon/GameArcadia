607ff4896daa8697f3e5040d537782edacdd5d24
EoinConcannon <G00406729@atu.ie>
2025-04-17 14:39:21 +0100

    small edit to test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// This is needed to mock CSS modules
+jest.mock('identity-obj-proxy', () => ({}));
+
+// Mock the CSS imports
+jest.mock('../src/App.css', () => ({}), { virtual: true });
+jest.mock('../styles/CheckOutPage.css', () => ({}), { virtual: true });
+jest.mock('../styles/AuthPages.css', () => ({}), { virtual: true });
+jest.mock('../styles/ProfilePage.css', () => ({}), { virtual: true });
+jest.mock('../styles/FrontPage.css', () => ({}), { virtual: true });
+jest.mock('../styles/StorePage.css', () => ({}), { virtual: true });
+jest.mock('bootstrap/dist/css/bootstrap.min.css', () => ({}), { virtual: true });
